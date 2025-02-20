cdab840e352549c9995fb31a4ffe505d58777948
Kavindu Liyanage <95487550+Kavindu80@users.noreply.github.com>
2025-02-20 14:36:10 +0530

    Update applicationSlice.js

--- frontend/src/store/slices/applicationSlice.js
+++ frontend/src/store/slices/applicationSlice.js
@@ -84,7 +84,7 @@ export const fetchEmployerApplications = () => async (dispatch) => {
   dispatch(applicationSlice.actions.requestForAllApplications());
   try {
     const response = await axios.get(
-      `http://44.223.250.83:4000/api/v1/application/employer/getall`,
+      `http://100.28.92.187:4000/api/v1/application/employer/getall`,
       {
         withCredentials: true,
       }
@@ -108,7 +108,7 @@ export const fetchJobSeekerApplications = () => async (dispatch) => {
   dispatch(applicationSlice.actions.requestForMyApplications());
   try {
     const response = await axios.get(
-      `http://44.223.250.83:4000/api/v1/application/jobseeker/getall`,
+      `http://100.28.92.187:4000/api/v1/application/jobseeker/getall`,
       {
         withCredentials: true,
       }
@@ -132,7 +132,7 @@ export const postApplication = (data, jobId) => async (dispatch) => {
   dispatch(applicationSlice.actions.requestForPostApplication());
   try {
     const response = await axios.post(
-      `http://44.223.250.83:4000/api/v1/application/post/${jobId}`,
+      `http://100.28.92.187:4000/api/v1/application/post/${jobId}`,
       data,
       {
         withCredentials: true,
@@ -156,7 +156,7 @@ export const deleteApplication = (id) => async (dispatch) => {
   dispatch(applicationSlice.actions.requestForDeleteApplication());
   try {
     const response = await axios.delete(
-      `http://44.223.250.83:4000/api/v1/application/delete/${id}`,
+      `http://100.28.92.187:4000/api/v1/application/delete/${id}`,
       { withCredentials: true }
     );
     dispatch(
