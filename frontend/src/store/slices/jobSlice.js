3a7fd819e68b319362c11043285e28c8b915af8c
Kavindu Liyanage <95487550+Kavindu80@users.noreply.github.com>
2025-02-20 14:36:59 +0530

    Update jobSlice.js

--- frontend/src/store/slices/jobSlice.js
+++ frontend/src/store/slices/jobSlice.js
@@ -108,7 +108,7 @@ export const fetchJobs =
   async (dispatch) => {
     try {
       dispatch(jobSlice.actions.requestForAllJobs());
-      let link = "http://44.223.250.83:4000/api/v1/job/getall?";
+      let link = "http://100.28.92.187:4000/api/v1/job/getall?";
       let queryParams = [];
       if (searchKeyword) {
         queryParams.push(`searchKeyword=${searchKeyword}`);
@@ -157,7 +157,7 @@ export const fetchSingleJob = (jobId) => async (dispatch) => {
   dispatch(jobSlice.actions.requestForSingleJob());
   try {
     const response = await axios.get(
-      `http://44.223.250.83:4000/api/v1/job/get/${jobId}`,
+      `http://100.28.92.187:4000/api/v1/job/get/${jobId}`,
       { withCredentials: true }
     );
     dispatch(jobSlice.actions.successForSingleJob(response.data.job));
@@ -171,7 +171,7 @@ export const postJob = (data) => async (dispatch) => {
   dispatch(jobSlice.actions.requestForPostJob());
   try {
     const response = await axios.post(
-      `http://44.223.250.83:4000/api/v1/job/post`,
+      `http://100.28.92.187:4000/api/v1/job/post`,
       data,
       { withCredentials: true, headers: { "Content-Type": "application/json" } }
     );
@@ -186,7 +186,7 @@ export const getMyJobs = () => async (dispatch) => {
   dispatch(jobSlice.actions.requestForMyJobs());
   try {
     const response = await axios.get(
-      `http://44.223.250.83:4000/api/v1/job/getmyjobs`,
+      `http://100.28.92.187:4000/api/v1/job/getmyjobs`,
       { withCredentials: true }
     );
     dispatch(jobSlice.actions.successForMyJobs(response.data.myJobs));
@@ -200,7 +200,7 @@ export const deleteJob = (id) => async (dispatch) => {
   dispatch(jobSlice.actions.requestForDeleteJob());
   try {
     const response = await axios.delete(
-      `http://44.223.250.83:4000/api/v1/job/delete/${id}`,
+      `http://100.28.92.187:4000/api/v1/job/delete/${id}`,
       { withCredentials: true }
     );
     dispatch(jobSlice.actions.successForDeleteJob(response.data.message));
