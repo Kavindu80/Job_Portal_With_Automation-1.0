2f18b8a8721c188cf6ca7556183e674e77371977
Kavindu80 <kavinduraing@gmail.com>
2025-02-20 14:03:42 +0530

    initialized

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer></footer>
+      <div className="copyright">
+        &copy; CopyRight 2025. All Rights Reserved By Kavindu
+      </div>
+    </>
+  );
+};
+
+export default Footer;
